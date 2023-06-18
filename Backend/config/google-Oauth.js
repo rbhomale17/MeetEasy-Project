7a8e5f13382f6880f7e667cad9dbb5ade48bb30b
rbhomale17 <rushikeshbhomale@gmail.com>
2023-06-18 17:43:09 +0530

    another setup commit after replacing local host to url

--- Backend/config/google-Oauth.js
+++ Backend/config/google-Oauth.js
@@ -10,8 +10,7 @@ const { UserModel } = require('../models/user.model');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    // callbackURL: "http://localhost:3000/auth/google/callback"
-    callbackURL: `${Base_url}/auth/google/callback`
+    callbackURL: `https://meeteasy-main-server.onrender.com/auth/google/callback`
   },
  async function(accessToken, refreshToken, profile, cb) {
     // User.findOrCreate({ googleId: profile.id }, function (err, user) {
