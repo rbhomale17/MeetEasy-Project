86ee84d4062a2cb0bec50364793cc18db980f0fb
rbhomale17 <rushikeshbhomale@gmail.com>
2023-06-20 23:58:09 +0530

    Oauth service working

--- landing/Backend/routes/auth.routes.js
+++ landing/Backend/routes/auth.routes.js
@@ -49,7 +49,7 @@ passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
+      callbackURL: "https://meeteasy-main-server.onrender.com/auth/google/callback",
       passReqToCallback: true,
     },
     async function (request, accessToken, refreshToken, profile, done) {
